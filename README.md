ecb8bd0fb38293e63e0a67187e72da25d3f1cfb2
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-27 01:31:32 +0800

    Introduction

--- README.md
+++
@@ -1,2 +0,0 @@
-# WRITING-MULTIPLE-TEXT-CONTENTS-IN-A-FILE
-a method in python to write multiple line of text contents into a text file
---
+++ Writing_Lines_Into_File.py
@@ -0,0 +1,4 @@
+ #BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 1
+ #Write a method in python to write multiple line of text contents into a text file mylife.txt.
+ 
+ 
