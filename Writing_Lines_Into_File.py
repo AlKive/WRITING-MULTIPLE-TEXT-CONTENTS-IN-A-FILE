96a2b5404d32347fa3b41a9ba452b1e5f7eb5e5c
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-27 18:58:12 +0800

    PSEUDOCODE

--- Writing_Lines_Into_File.py
+++ Writing_Lines_Into_File.py
@@ -1,4 +1,25 @@
- #BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 1
+ #BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 3
  #Write a method in python to write multiple line of text contents into a text file mylife.txt.
  
+ #BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 3
+ #Write a method in python to write multiple line of text contents into a text file mylife.txt.
  
+'''PSEODOCODE
+ #CREATE a function
+ #GET user's decision to type a line
+ #WHILE user's decision is not blank,proceed
+          #IF user's decision == YES
+             #CREATEE/OPEN the file
+             #GET user's input
+             #APPEND the input into the file
+             #CONTINUE asking to add more lines
+             
+          #ELIF user's decision == NO   
+              #EXIT()
+              
+          #ELIF user's decision is not a YES or NO    
+              #KEEP PROMPTING the user until they enter correctly
+              
+          #ELSE break    
+#START
+#function() '''
