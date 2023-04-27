45106f658f2fd627b484f2089b8f06470320191e
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-27 19:02:54 +0800

    Starting the function

--- Writing_Lines_Into_File.py
+++ Writing_Lines_Into_File.py
@@ -4,10 +4,13 @@
  #BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 3
  #Write a method in python to write multiple line of text contents into a text file mylife.txt.
  
-'''PSEODOCODE
- #CREATE a function
+
+#CREATE a function
+def _yes_or_no():
  #GET user's decision to type a line
+ enter_again = input("Do you want to add lines?  YES or NO? : ")
  #WHILE user's decision is not blank,proceed
+ while len(enter_again) != 0 or len(enter_again)!= 1 :
           #IF user's decision == YES
              #CREATEE/OPEN the file
              #GET user's input
@@ -22,4 +25,4 @@
               
           #ELSE break    
 #START
-#function() '''
+#function()
